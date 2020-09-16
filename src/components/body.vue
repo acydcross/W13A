e351a6234f08f790d1f523a55b04f78cfe4a9d5a
acydcross <acydcross@hotmail.com>
2020-09-15 21:05:37 -0600

    First commit

---
+++ src/components/body.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app2" class="page-body">
+        <div></div>
+        <h3>BODY</h3>
+        <h3>BODY</h3>
+        <h3>BODY</h3>
+        <div></div>
+        <page-left></page-left>
+        <page-body2></page-body2>
+        <page-right></page-right>
+    </div>
+</template>
+
+<script>
+import PageLeft from "./components/left.vue";
+import PageSecondBody from "./components/body2.vue";
+import PageRight from "./components/right.vue";
+
+export default {
+    name: "page-footer",
+    components: {
+    PageLeft,
+    PageSecondBody,
+    PageRight
+  }
+}
+</script>
+
+<style scoped>
+    .page-body {
+        display: grid;
+        justify-items: center;
+        align-items: center;
+        grid-template-columns: 1fr 2fr 1fr 1fr 1fr;
+    }
+</style>
---
+++ src/components/body2.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="page-body2">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <div></div>
+        <h3>BODY2</h3>
+        <h3>BODY2</h3>
+        <h3>BODY2</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "page-body2"
+    }
+</script>
+
+<style scoped>
+    .another-body {
+        display: grid;
+        justify-items: center;
+        align-items: center;
+        grid-template-columns: 1fr 2fr 1fr 1fr 1fr;
+    }
+</style>
